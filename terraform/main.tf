0f1180312a820bde984629112ffc8fbd158c7a91
William Schweitzer <weschweitzer@outlook.com>
2025-11-22 17:13:28 -0500

    fix(terraform):allow connections from ECS to taskmanagement RDS

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,17 +61,26 @@ resource "aws_security_group" "rds" {
   name   = "taskmanagement_rds"
   vpc_id = module.vpc.vpc_id
 
+  # Allow from ECS tasks
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_tasks.id]  # Reference ECS security group
   }
 
+  # Public access not needed
+  # ingress {
+  #   from_port   = 5432
+  #   to_port     = 5432
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
